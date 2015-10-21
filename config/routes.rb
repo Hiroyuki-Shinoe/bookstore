1db8a4650315f6034a2c5ad554b0544861c91f84
yuxing <yuxing0129@gmail.com>
2015-10-21 09:13:10 +0900

    Added controllers and refactored routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :publishers
+
+  resources :authors
+
+  resources :categories
+
+  resources :books
+
+  get 'about/index' => 'about#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
